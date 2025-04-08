f83890f6b2b1ca51e7b26e384f63995df0a3c0f8
Anonymous <anonymous@example.com>
2025-04-07 21:44:15 -0300

    Configurando o Back do MainActivity

--- app/src/main/java/com/example/userregistrationapp/MainActivity.java
+++ app/src/main/java/com/example/userregistrationapp/MainActivity.java
@@ -1,24 +1,62 @@
 package com.example.userregistrationapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.room.Room;
 
+//Classe para verificar erros com log
+import android.util.Log;
+
+
+//Classe principal da atividade de cadastro de usuarios
 public class MainActivity extends AppCompatActivity {
 
+//    Declara os campos de entrada de dados
+    private EditText editTextName, editTextCPF , editTextEmail , editTextPhone;
+
+//    Cria objeto para interagir com o banco de dados(dao)
+    private UserDao userDao;
+
+//    Metodo chamado quanto a atividade é criada
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main); //Define o layout da tela
+
+//        Inicializa os campos de entrada de dados do layout
+        editTextName  = findViewById(R.id.editTextName);
+        editTextCPF   = findViewById(R.id.editTextCPF);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPhone = findViewById(R.id.editTextPhone);
+
+//        Inicializa os botoes do layout
+
+        Button buttonSave = findViewById(R.id.editButtonSave);
+        Button buttonReport = findViewById(R.id.editButtonReport);
+
+//        Configuracao do banco de dados usando Room
+        UserDatabase db = Room.databaseBuilder(getApplicationContext(),
+                UserDatabase.class, "user-database").allowMainThreadQueries().build();
+        userDao = db.userDao(); // Obtem uma instancia do DAO para interagir com os dados
+
+//        Configura o botao de salvar do usuario
+        buttonSave.setOnClickListener(v -> {
+//            Configura que o click aconteceu
+            Log.d("MainActivity" , "Botão Cadastrar Usuário clicado");
+
+//            Coleta dados digitados pelo usuario
+            String name = editTextName.getText().toString();
+            String cpf  = editTextCPF.getText().toString();
+            String email = editTextEmail.getText().toString();
+            String phone = editTextPhone.getText().toString();
+
+
+
         });
+
     }
 }
